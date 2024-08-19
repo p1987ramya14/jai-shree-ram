a906d2f46eccaa11f7a9ae38cc8644f03e8b84be
p1987ramya14 <167653518+p1987ramya14@users.noreply.github.com>
2024-08-19 17:57:47 +0530

    Update index.php

--- click/index.php
+++ click/index.php
@@ -10,8 +10,8 @@ function getUserIP() {
 }
 
 $userIP = getUserIP();
-$accessKey = getenv('69d1c6a59b8cfb849eceea17a8d2a2c2'); // Ensure this environment variable is set
-$geoDataUrl = "http://api.ipstack.com/$userIP?access_key=$accessKey";
+$accessKey = '69d1c6a59b8cfb849eceea17a8d2a2c2'; // Replace with your actual API key
+$geoDataUrl = "https://api.ipinfo.io/{$userIP}?access_key={$accessKey}";
 
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, $geoDataUrl);
@@ -38,9 +38,9 @@ if (json_last_error() !== JSON_ERROR_NONE) {
     exit();
 }
 
-error_log('Geo Data: ' . print_r($geoData, true));
+error_log('Decoded Geo Data: ' . print_r($geoData, true));
 
-if (isset($geoData['country_code']) && $geoData['country_code'] === 'US') {
+if (isset($geoData['country']) && strtoupper($geoData['country']) === 'US') {
     header("Location: https://roastandrelish.store");
 } else {
     header("Location: https://www.amazon.com/Simple-Joys-Carters-Short-Sleeve-Bodysuit/dp/B07GY1RRZF");
